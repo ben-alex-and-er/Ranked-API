395026b0da9a565a28a97434dae9f28eda4d413b
Ben Alexander <ben250402@yahoo.co.uk>
2025-07-19 17:02:04 +0100

    1 Added UNIT Tests for UserService

---
+++ Unit Tests/Services/User/UserServiceTestCaseSources.cs
@@ -0,0 +1,21 @@
+﻿using Ranked.Data.User.Status;
+
+
+namespace Unit_Tests.Services.User
+{
+	using Data;
+
+
+	internal static class UserServiceTestCaseSources
+	{
+		public static IEnumerable<TestCaseData> CreateTestCaseSource
+		{
+			get
+			{
+				yield return new TestCaseData(UserConsts.NEW_USER_1, CreateUserStatus.SUCCESS);
+				yield return new TestCaseData(UserConsts.VALID_USER_1, CreateUserStatus.USER_ALREADY_EXISTS);
+				yield return new TestCaseData(UserConsts.INVALID_USER_1, CreateUserStatus.FAILED_TO_CREATE_ELO);
+			}
+		}
+	}
+}
