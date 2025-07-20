bb3ae47be7c9cedfa6b2570d2ffcb478dc15eee8
Ben Alexander <ben250402@yahoo.co.uk>
2025-07-20 09:30:35 +0100

    1 Added UNIT tests for UserEloDA

---
+++ Unit Tests/DataAccessors/Elo/UserEloDATestCaseSources.cs
@@ -0,0 +1,28 @@
+﻿namespace Unit_Tests.DataAccessors.Elo
+{
+	using Data;
+
+
+	internal class UserEloDATestCaseSources
+	{
+		public static IEnumerable<TestCaseData> CreateTestCaseSource
+		{
+			get
+			{
+				yield return new TestCaseData(UserConsts.NEW_USER_1, 1000, false);
+				yield return new TestCaseData(UserConsts.VALID_USER_1, 1000, false);
+				yield return new TestCaseData(UserConsts.VALID_USER_4, 4000, true);
+			}
+		}
+
+		public static IEnumerable<TestCaseData> UpdateTestCaseSource
+		{
+			get
+			{
+				yield return new TestCaseData(UserConsts.NEW_USER_1, 1000, false);
+				yield return new TestCaseData(UserConsts.VALID_USER_4, 4000, false);
+				yield return new TestCaseData(UserConsts.VALID_USER_1, 2000, true);
+			}
+		}
+	}
+}
