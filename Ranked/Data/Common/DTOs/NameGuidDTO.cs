d9907ba0e95f3378a91e16cda0ff0851a60dfdf4
Ben Alexander <ben250402@yahoo.co.uk>
2025-09-06 19:29:52 +0100

    3 Added Equals to NameGuidDTO

--- Ranked/Data/Common/DTOs/NameGuidDTO.cs
+++ Ranked/Data/Common/DTOs/NameGuidDTO.cs
@@ -14,5 +14,17 @@
 		/// Guid of the item
 		/// </summary>
 		public Guid Guid { get; set; }
+
+
+		public override bool Equals(object? obj)
+		{
+			if (obj is not NameGuidDTO other)
+				return false;
+
+			return other.Name == Name && other.Guid == Guid;
+		}
+
+		public override int GetHashCode()
+			=> HashCode.Combine(Name, Guid);
 	}
 }
