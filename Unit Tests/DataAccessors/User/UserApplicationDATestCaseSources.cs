36c0f7b994001b4cbed857c371fe2f505da510ad
Ben Alexander <ben250402@yahoo.co.uk>
2025-09-06 19:20:46 +0100

    3 Added UNIT tests for UserApplicationDA

---
+++ Unit Tests/DataAccessors/User/UserApplicationDATestCaseSources.cs
@@ -0,0 +1,18 @@
+﻿namespace Unit_Tests.DataAccessors.User
+{
+	using Data;
+
+
+	internal static class UserApplicationDATestCaseSources
+	{
+		public static IEnumerable<TestCaseData> CreateTestCaseSource
+		{
+			get
+			{
+				yield return new TestCaseData(UserConsts.NEW_USER_1, AppConsts.APP_GUID, true);
+				yield return new TestCaseData(UserConsts.VALID_USER_1, AppConsts.APP_GUID, false);
+				yield return new TestCaseData(UserConsts.NEW_USER_1, Guid.Empty.ToString(), false);
+			}
+		}
+	}
+}
