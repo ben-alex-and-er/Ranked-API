c99615ecc88b205e23b68be69ad2b1d761aaba4d
Ben Alexander <ben250402@yahoo.co.uk>
2025-09-07 09:41:39 +0100

    4 Added UNIT Tests for SecurityService

---
+++ Unit Tests/Data/AuthConsts.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+
+namespace Unit_Tests.Data
+{
+	internal class AuthConsts
+	{
+		public const string SUBJECT = "Subject";
+		public const string PASSWORD = "password";
+
+		public const string CLAIM_TYPE = "Claim Type";
+		public const string CLAIM_VALUE = "Claim Value";
+
+		public const string JWT_ISSUER = "Issuer";
+		public const string JWT_AUDIENCE = "Audience";
+		public const string JWT_SECURITY_KEY = "Security Key - This needs to be at least 128 bits long";
+
+
+		public static Claim Claim = new(CLAIM_TYPE, CLAIM_VALUE);
+	}
+}
---
+++ Unit Tests/ProviderCreators/Authorization/AuthorizationProviderCreator.cs
@@ -0,0 +1,42 @@
+﻿using GateKeeper.Cryptography;
+using NSubstitute;
+using Ranked.Providers.Authorization.Interfaces;
+using System.Security.Claims;
+using TestRig.Extensions;
+
+
+namespace Unit_Tests.ProviderCreators.Authorization
+{
+	using Data;
+
+
+	internal static class AuthorizationProviderCreator
+	{
+		public static IClaimsProvider CreateClaimsProvider()
+		{
+			var provider = Substitute.For<IClaimsProvider>();
+
+			provider.GetClaims(Arg.Any<string>())
+				.Returns(new List<Claim>()
+				{
+					AuthConsts.Claim
+				}.AsEFTestQueryable());
+
+			return provider;
+		}
+
+
+		public static IRoleProvider CreateRoleProvider()
+		{
+			var provider = Substitute.For<IRoleProvider>();
+
+			provider.GetRoles()
+				.Returns(new Dictionary<string, string>()
+				{
+					{ AuthConsts.SUBJECT, PasswordHasher.HashPassword(AuthConsts.PASSWORD).ToString() }
+				});
+
+			return provider;
+		}
+	}
+}
